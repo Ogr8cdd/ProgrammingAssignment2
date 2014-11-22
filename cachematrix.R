bc82033e12ca27873521cf19e99b32ab5ff126ad
dugdacli <clived@pokerstars.com>
2014-11-22 15:19:50 +0000

    This is the first part of my assignment.
    No proper testing done yet.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m_mat <- NULL
+  
+  get <- function(){x}
+  set <- function(y) {
+      x <<- y
+      m_mat <<- NULL
+    }
+  setInv <- function(solve) {m_mat <<- solve}
+  getInv <- function() {m_mat}
+  list (get =get, set = set, setInv = setInv, getInv = getInv)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    mat <- x$getInv()
+    if(!is.null(mat)){
+      return(mat)
+    }
+    disp_mat <- x$get()
+    inv <- solve(disp_mat)    
+    x$setInv(inv)
+    inv
 }
